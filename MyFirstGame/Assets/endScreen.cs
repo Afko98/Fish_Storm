4f3c65276408b2c78160a689d1b21e7b5168cdbc
Afko98 <miki.velic@hotmail.com>
2021-07-25 13:49:24 +0200

    25_13:49

---
+++ MyFirstGame/Assets/endScreen.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class endScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    
+
+    // Update is called once per frame
+    public void Update()
+    {
+            gameObject.SetActive(true);
+    }
+}
